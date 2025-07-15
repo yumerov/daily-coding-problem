d1e0108a3c3bcb4c132ecc3f5a4c454eb80245ff
yumerov <yumerov.levent@gmail.com>
2025-07-15 22:00:36 +0300

    [DCP-3] recursive tree class

---
+++ AlphabetMappedNumericStrings/RecursiveDictionary.cs
@@ -0,0 +1,27 @@
+namespace AlphabetMappedNumericStrings;
+
+/// <summary>
+/// Recursive dictionary used as a tree
+/// </summary>
+public class RecursiveDictionary<T> : Dictionary<T, RecursiveDictionary<T>?> where T : notnull
+{
+    public int LeafCount
+    {
+        get
+        {
+            var count = 0;
+            foreach (var child in Values)
+            {
+                if (child == null || child.Count == 0)
+                {
+                    count++;
+                    continue;
+                }
+
+                count += child.LeafCount;
+            }
+
+            return count;
+        }
+    }
+}
