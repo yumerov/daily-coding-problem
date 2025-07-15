bf4ea564e26598d3c3d171477f061dd1477d2f91
yumerov <yumerov.levent@gmail.com>
2025-07-15 23:49:49 +0300

    [DCP-3][WIP] single char string handling

--- AlphabetMappedNumericStrings.Tests/AlphabetMappedNumericDecoderTests.cs
+++ AlphabetMappedNumericStrings.Tests/AlphabetMappedNumericDecoderTests.cs
@@ -13,29 +13,26 @@ public class AlphabetMappedNumericDecoderTests
     [InlineData("7", 1)]
     [InlineData("8", 1)]
     [InlineData("9", 1)]
-    public void OneDigit(string str, int count)
-    {
-        Assert.Equal(count, AlphabetMappedNumericDecoder.GetCount(str));
-    }
-    
+    public void OneDigit(string str, int count) => AssertCount(count, str);
+
     [Theory]
     [InlineData("10", 1)]
-    [InlineData("11", 3)]
-    [InlineData("12", 3)]
-    [InlineData("13", 3)]
-    [InlineData("14", 3)]
-    [InlineData("15", 3)]
-    [InlineData("16", 3)]
-    [InlineData("17", 3)]
-    [InlineData("18", 3)]
-    [InlineData("19", 3)]
+    [InlineData("11", 2)]
+    [InlineData("12", 2)]
+    [InlineData("13", 2)]
+    [InlineData("14", 2)]
+    [InlineData("15", 2)]
+    [InlineData("16", 2)]
+    [InlineData("17", 2)]
+    [InlineData("18", 2)]
+    [InlineData("19", 2)]
     [InlineData("20", 1)]
-    [InlineData("21", 3)]
-    [InlineData("22", 3)]
-    [InlineData("23", 3)]
-    [InlineData("24", 3)]
-    [InlineData("25", 3)]
-    [InlineData("26", 3)]
+    [InlineData("21", 2)]
+    [InlineData("22", 2)]
+    [InlineData("23", 2)]
+    [InlineData("24", 2)]
+    [InlineData("25", 2)]
+    [InlineData("26", 2)]
     [InlineData("27", 2)]
     [InlineData("28", 2)]
     [InlineData("29", 2)]
@@ -48,13 +45,20 @@ public class AlphabetMappedNumericDecoderTests
     [InlineData("60", 1)]
     [InlineData("64", 2)]
     [InlineData("70", 1)]
+    [InlineData("71", 2)]
     [InlineData("75", 2)]
     [InlineData("80", 1)]
     [InlineData("86", 2)]
     [InlineData("90", 1)]
     [InlineData("97", 2)]
-    public void TwoDigits(string str, int count)
-    {
-        Assert.Equal(count, AlphabetMappedNumericDecoder.GetCount(str));
-    }
+    public void TwoDigits(string str, int count) => AssertCount(count, str);
+    
+    [Theory]
+    [InlineData("101", 1)]
+    [InlineData("111", 3)]
+    [InlineData("271", 1)]
+    public void ThreeDigits(string str, int count) => AssertCount(count, str);
+    
+    private static void AssertCount(int count, string str) =>
+        Assert.Equal(count, new AlphabetMappedNumericDecoder(str).BuildTree().CombinationCount);
 }
--- AlphabetMappedNumericStrings/AlphabetMappedNumericDecoder.cs
+++ AlphabetMappedNumericStrings/AlphabetMappedNumericDecoder.cs
@@ -1,35 +1,76 @@
 namespace AlphabetMappedNumericStrings;
 
-public static class AlphabetMappedNumericDecoder
+using RecursiveIntDictionary = RecursiveDictionary<int>;
+
+public class AlphabetMappedNumericDecoder(string value)
 {
-    public static int GetCount(string str)
+    private readonly string _value = value;
+    public int CombinationCount => _tree.LeafCount;
+    private readonly RecursiveIntDictionary _tree = new();
+    private readonly Dictionary<string, RecursiveIntDictionary> _cachedBranches = new();
+
+    public AlphabetMappedNumericDecoder BuildTree()
     {
-        if (str.Length == 1) return CountOneChar(str);
+        if (_value.Equals("0"))
+        {
+            return this;
+        }
+        
+        _tree[0] = BuildTree(_value);
 
-        if (str.Length == 2) return CountTwoChars(str);
-
-        return 0;
-
-        // var head = int.Parse(str[..2]);
-        //
-        // var tail = str[2..];
-        //
-        // if (head > 26)
-        // {
-        //     return GetCount(tail);
-        // }
-        //
-        // return GetCount(tail) + GetCount($"{head % 10}{tail}");
+        return this;
     }
+
+    private RecursiveIntDictionary BuildTree(string value)
+    {
+        // if (_cachedBranches.TryGetValue(value, out var tree))
+        // {
+        //     return tree;
+        // }
+
+        if (value.Equals("0"))
+        {
+            return new RecursiveIntDictionary();
+        }
+        
+        if (value.Length == 1)
+        {
+            return new RecursiveIntDictionary
+            {
+                [int.Parse(value)] = null
+            };
+        }
+
+
+        // if (value.Length == 2) return CountTwoChars(str);
+
+        var tail = value[2..];
+
+        return _cachedBranches[value];
+    }
+
+    // public int GetCount()
+    // {
+    //     if (str.Length == 1) return CountOneChar(str);
+    //
+    //     if (str.Length == 2) return CountTwoChars(str);
+    //     
+    //     var head = str[..2];
+    //     var tail = str[2..];
+    //     var headCombinations = CountTwoChars(head);
+    //     if (headCombinations == 1) return GetCount(tail);
+    //     if (headCombinations == 2) return GetCount(tail) + GetCount($"{head[1]}{tail}");
+    //     
+    //     return GetCount($"{head[1]}{tail}");
+    // }
 
     private static int CountOneChar(string str) => str[0] == '0' ? 0 : 1;
 
     private static int CountTwoChars(string str) => int.Parse(str[..2]) switch
     {
+        < 9 => 1,
         var n when n % 10 == 0 => 1,
-        >= 11 and <= 19 => 3,
-        20 => 1,
-        >= 21 and <= 26 => 3,
+        >= 11 and <= 19 or >= 21 and <= 26 => 2,
         _ => 2
     };
 }
